88de6d2f1d41da3b451e1c0087f4be553e2aa2c5
huangnan <foxstarx@gmail.com>
2018-09-14 14:30:06 +0800

    每页显示20条

--- demand/admin.py
+++ demand/admin.py
@@ -2,8 +2,20 @@ from django.contrib import admin
 from .models import human, demand
 
 class humanAdmin(admin.ModelAdmin):
-    empty_value_display = '- empty -'
-    list_display = ('name', 'company', 'deparment', 'phone', 'email')
+    empty_value_display = '-empty-'
+    list_display = ('name', 'human_type', 'company', 'deparment', 'title', 'phone', 'email')
+    list_per_page = 20
+    fieldsets = (
+        ['个人信息', {
+            'fields': ('name', 'human_type')
+        }], 
+        ['公司信息', {
+            'fields': ('company', 'deparment', 'title')
+        }], 
+        ['联系信息', {
+            'fields': ('phone', 'email')
+        }]
+    )
 
 admin.site.register(human, humanAdmin)
 admin.site.register(demand)
