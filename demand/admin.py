c31bc8971fa9a19bcaab282da89377e300eca8f9
huangnan <foxstarx@gmail.com>
2018-09-23 23:15:22 +0800

    完成模型日志记录

--- demand/admin.py
+++ demand/admin.py
@@ -1,11 +1,33 @@
 from django.contrib import admin
 from django.forms.models import model_to_dict
 from django.contrib.admin.models import LogEntry
-from .models import human_type, human, demand, demand_status
+from .models import human_type, human, demand, demand_status, priority
 
 import logging
 
 logger = logging.getLogger('ajms')
+
+@admin.register(priority)
+class priorityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color_code', 'description')
+    list_editable = ['color_code']
+
+    def log_change(self, request, object, message):
+        new_message = []
+        for item in message:
+            obj = item
+            obj["changed"]["values"] = object.to_dict()
+            new_message.append(obj)
+        super(priorityAdmin, self).log_change(request, object, new_message)
+
+    def log_addition(self, request, object, message):
+        new_message = []
+        value = {}
+        value["added"] = {}
+        value["added"]["values"] = object.to_dict()
+        new_message.append(value)
+        super(priorityAdmin, self).log_addition(request, object, new_message)
+
 
 @admin.register(human_type)
 class human_typeAdmin(admin.ModelAdmin):
@@ -17,15 +39,15 @@ class human_typeAdmin(admin.ModelAdmin):
         new_message = []
         for item in message:
             obj = item
-            obj["changed"]["values"] = model_to_dict(object)
+            obj["changed"]["values"] = object.to_dict()
             new_message.append(obj)
         super(human_typeAdmin, self).log_change(request, object, new_message)
 
-    def log_addition(self, request, object):
+    def log_addition(self, request, object, message):
         new_message = []
         value = {}
         value["added"] = {}
-        value["added"]["values"] = model_to_dict(object)
+        value["added"]["values"] = object.to_dict()
         new_message.append(value)
         super(human_typeAdmin, self).log_addition(request, object, new_message)
 
@@ -53,28 +75,41 @@ class humanAdmin(admin.ModelAdmin):
         new_message = []
         for item in message:
             obj = item
-            obj["changed"]["values"] = model_to_dict(object)
+            obj["changed"]["values"] = object.to_dict()
             new_message.append(obj)
         super(humanAdmin, self).log_change(request, object, new_message)
 
-    def log_addition(self, request, object):
+    def log_addition(self, request, object, message):
         new_message = []
         value = {}
         value["added"] = {}
-        value["added"]["values"] = model_to_dict(object)
+        value["added"]["values"] = object.to_dict()
         new_message.append(value)
+        print(new_message)
         super(humanAdmin, self).log_addition(request, object, new_message)
 
 @admin.register(demand)
 class demandAdmin(admin.ModelAdmin):
     empty_value_display = '-empty-'
-    list_display = ('name', 'colored_status', 'publish_date', 'publisher', 'priority', 'processor', 'percent')
+    list_display = ('need_auto', 'name', 'colored_priority', 'colored_status', 'publish_date', 'publisher', 'publisher_department', 'firster', 
+    'ask_end_date', 'plan_begin_date', 'plan_end_date', 'processor', 'percent')
     list_per_page = 20
+    readonly_fields = ('publish_date', )
     # fieldsets = ('name', 'publisher', 'publish_date', 'priority', 'processor', 'percent')
     fieldsets = (
         [
             '基本信息', {
-                'fields': (('name', 'priority'), ('publisher', 'processor'), 'percent', 'status')
+                'fields': (('name', 'need_auto'), ('priority', 'status'))
+            }
+        ], 
+        [
+            '需求方信息', {
+                'fields': (('publisher', 'firster'), 'ask_end_date')
+            }
+        ], 
+        [
+            '承接方信息', {
+                'fields': ('processor', ('plan_begin_date', 'plan_end_date'))
             }
         ], 
         [
@@ -83,24 +118,24 @@ class demandAdmin(admin.ModelAdmin):
             }
         ], 
     )
-    list_filter = ('publisher', 'processor', 'priority')
+    list_filter = ('priority', 'publisher', 'processor')
     search_fields = ('name', 'publisher', 'processor')
-    list_editable = ['percent','processor']
+    list_editable = ['percent', ]
     date_hierarchy = 'publish_date'
 
     def log_change(self, request, object, message):
         new_message = []
         for item in message:
             obj = item
-            obj["changed"]["values"] = model_to_dict(object)
+            obj["changed"]["values"] = object.to_dict()
             new_message.append(obj)
         super(demandAdmin, self).log_change(request, object, new_message)
 
-    def log_addition(self, request, object):
+    def log_addition(self, request, object, message):
         new_message = []
         value = {}
         value["added"] = {}
-        value["added"]["values"] = model_to_dict(object)
+        value["added"]["values"] = object.to_dict()
         new_message.append(value)
         super(demandAdmin, self).log_addition(request, object, new_message)
 
@@ -114,15 +149,15 @@ class demand_statusAdmin(admin.ModelAdmin):
         new_message = []
         for item in message:
             obj = item
-            obj["changed"]["values"] = model_to_dict(object)
+            obj["changed"]["values"] = object.to_dict()
             new_message.append(obj)
         super(demand_statusAdmin, self).log_change(request, object, new_message)
 
-    def log_addition(self, request, object):
+    def log_addition(self, request, object, message):
         new_message = []
         value = {}
         value["added"] = {}
-        value["added"]["values"] = model_to_dict(object)
+        value["added"]["values"] = object.to_dict()
         new_message.append(value)
         super(demand_statusAdmin, self).log_addition(request, object, new_message)
 
--- demand/models.py
+++ demand/models.py
@@ -1,21 +1,49 @@
 import logging
+import datetime
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.html import format_html
+from django.utils import timezone
 
 from comm.models import color_info
 from .enums import enum_human_type, enum_human_type_info
 
 logger = logging.getLogger('ajms')
 
-class human_type(models.Model):
-    name = models.CharField(max_length = 100, blank = False, verbose_name = '类型名称')
-    color_code = models.ForeignKey(color_info, blank = False, on_delete = models.SET('000000'), verbose_name = '颜色代码')
+class priority(models.Model):
+    name = models.CharField(max_length = 100, blank = True, verbose_name = '优先级名称')
+    color_code = models.ForeignKey(color_info, blank = True, on_delete = models.PROTECT, verbose_name = '颜色代码')
     description = models.TextField(max_length = 2000, blank = True, verbose_name = '描述')
 
     def __str__(self):
         return self.name
+
+    def to_dict(self):
+        result = {}
+        result["name"] = self.name
+        result["color_code"] = self.color_code.pk
+        result["description"] = self.description
+        return result
+
+    class Meta:
+        verbose_name = '优先级'
+        verbose_name_plural = '优先级'
+
+class human_type(models.Model):
+    name = models.CharField(max_length = 100, blank = False, verbose_name = '类型名称')
+    color_code = models.ForeignKey(color_info, blank = False, on_delete = models.PROTECT, verbose_name = '颜色代码')
+    description = models.TextField(max_length = 2000, blank = True, verbose_name = '描述')
+
+    def __str__(self):
+        return self.name
+
+    def to_dict(self):
+        result = {}
+        result["name"] = self.name
+        result["color_code"] = self.color_code.pk
+        result["description"] = self.description
+        return result
 
     class Meta:
         verbose_name = '参与人类型'
@@ -32,6 +60,17 @@ class human(models.Model):
 
     def __str__(self):
         return self.name
+
+    def to_dict(self):
+        result = {}
+        result["company"] = self.company
+        result["deparment"] = self.deparment
+        result["name"] = self.name
+        result["title"] = self.title
+        result["humantype"] = self.humantype.pk
+        result["phone"] = self.phone
+        result["email"] = str(self.email)
+        return result
 
     def colored_human_type(self):
         return format_html(
@@ -58,24 +97,57 @@ class demand_status(models.Model):
     def __str__(self):
         return self.name
 
+    def to_dict(self):
+        result = {}
+        result["name"] = self.name
+        result["color_code"] = self.color_code.pk
+        result["description"] = self.description
+        return result
+
     class Meta:
         verbose_name = '需求状态'
         verbose_name_plural = '需求状态'
 
 
 class demand(models.Model):
+    need_auto = models.BooleanField(verbose_name = '固化', default = True)
     name = models.CharField(max_length = 100, blank = False, verbose_name = '需求名称')
-    publisher = models.ForeignKey(human, on_delete = models.CASCADE, related_name = '需求人', verbose_name = '需求人')
+    firster = models.ForeignKey(human, on_delete = models.PROTECT, related_name = '甲方对接人', verbose_name = '甲方对接人')
+    publisher = models.ForeignKey(human, on_delete = models.PROTECT, related_name = '需求人', verbose_name = '需求人')
     publish_date = models.DateField(auto_now_add = True, verbose_name = '需求日期')
-    priority = models.IntegerField(default = '4', verbose_name = '优先级')
-    processor = models.ForeignKey(human, on_delete = models.CASCADE, related_name = '承接人', verbose_name = '承接人')
-    percent = models.IntegerField(blank = True, verbose_name = '进度')
+    ask_end_date = models.DateField(verbose_name = '要求完成日期', blank = True, default = timezone.now)
+    plan_begin_date = models.DateField(verbose_name = '计划开发日期', blank = True, default = timezone.now)
+    plan_end_date = models.DateField(verbose_name = '计划完成日期', blank = True, default = timezone.now)
+    priority = models.ForeignKey(priority, on_delete = models.PROTECT, verbose_name = '优先级')
+    processor = models.ForeignKey(human, on_delete = models.PROTECT, related_name = '承接人', verbose_name = '承接人')
+    percent = models.IntegerField(blank = True, verbose_name = '进度', default = 0)
     # status = models.CharField(max_length = 10, blank = False, choices = enum_demand_status, verbose_name = '状态')
     status = models.ForeignKey(demand_status, on_delete = models.PROTECT, related_name = '需求', verbose_name = '状态')
     description = models.TextField(max_length = 2000, blank = True, verbose_name = '描述')
 
     def __str__(self):
         return self.name
+
+    def to_dict(self):
+        result = {}
+        result["need_auto"] = self.need_auto
+        result["name"] = self.name
+        result["firster"] = self.firster.pk
+        result["publisher"] = self.publisher.pk
+        result["publish_date"] = str(self.publish_date)
+        result["ask_end_date"] = str(self.ask_end_date)
+        result["plan_begin_date"] = str(self.plan_begin_date)
+        result["plan_end_date"] = str(self.plan_end_date)
+        result["priority"] = self.priority.pk
+        result["processor"] = self.processor.pk
+        result["percent"] = self.percent
+        result["status"] = self.status.pk
+        result["description"] = self.description
+        return result
+
+    def publisher_department(self):
+        return self.publisher.deparment
+    publisher_department.short_description = '需求部门'
 
     def colored_status(self):
         return format_html(
@@ -84,6 +156,14 @@ class demand(models.Model):
             self.status.name,
         )
     colored_status.short_description = '状态'
+
+    def colored_priority(self):
+        return format_html(
+            '<span style="color: #{};font-weight:bold">{}</span>',
+            self.priority.color_code.color_code,
+            self.priority.name,
+        )
+    colored_priority.short_description = '优先级'
 
     class Meta:
         verbose_name = '需求'
